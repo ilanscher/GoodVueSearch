2a14bfbc9b7cdc6a3c7143cd6a9455a0a3bb7ad3
ilanscher <ilan@nodeside.com>
2022-03-14 10:47:56 +0200

    add files 14-3

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,210 @@
+
+<template>
+  <v-simple-table>
+    <template v-slot:default>
+      <v-card>
+        <div class="active-cyan-3 active-cyan-4 mb-3">
+          <input
+            class="mt-5 form-control"
+            v-model="search"
+            type="text"
+            placeholder="Search"
+          />
+        </div>
+      </v-card>
+
+      <tbody>
+        <tr v-for="(item, index) in filterColor" :key="index">
+          <td :style="{ color: [item] }">{{ item }}</td>
+          <td>
+            <button
+              class="btn btn-danger btn-sm"
+              @click="deleteFromList(index)"
+            >
+              Delete
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+
+
+<script>
+export default {
+  name: "searchBar",
+  // props: {
+  //   color: String,
+  // },
+  data() {
+    return {
+      search: "",
+
+      list: [
+        "Black",
+        "Navy",
+        "DarkBlue",
+        "MediumBlue",
+        "Blue",
+        "DarkGreen",
+        "Green",
+        "Teal",
+        "DarkCyan",
+        "DeepSkyBlue",
+        "DarkTurquoise",
+        "MediumSpringGreen",
+        "Lime",
+        "SpringGreen",
+        "Aqua",
+        "Cyan",
+        "MidnightBlue",
+        "DodgerBlue",
+        "LightSeaGreen",
+        "ForestGreen",
+        "SeaGreen",
+        "DarkSlateGray",
+        "DarkSlateGrey",
+        "LimeGreen",
+        "MediumSeaGreen",
+        "Turquoise",
+        "RoyalBlue",
+        "SteelBlue",
+        "DarkSlateBlue",
+        "MediumTurquoise",
+        "Indigo",
+        "DarkOliveGreen",
+        "CadetBlue",
+        "CornflowerBlue",
+        "RebeccaPurple",
+        "MediumAquaMarine",
+        "DimGray",
+        "DimGrey",
+        "SlateBlue",
+        "OliveDrab",
+        "SlateGray",
+        "SlateGrey",
+        "LightSlateGray",
+        "LightSlateGrey",
+        "MediumSlateBlue",
+        "LawnGreen",
+        "Chartreuse",
+        "Aquamarine",
+        "Maroon",
+        "Purple",
+        "Olive",
+        "Gray",
+        "Grey",
+        "SkyBlue",
+        "LightSkyBlue",
+        "BlueViolet",
+        "DarkRed",
+        "DarkMagenta",
+        "SaddleBrown",
+        "DarkSeaGreen",
+        "LightGreen",
+        "MediumPurple",
+        "DarkViolet",
+        "PaleGreen",
+        "DarkOrchid",
+        "YellowGreen",
+        "Sienna",
+        "Brown",
+        "DarkGray",
+        "DarkGrey",
+        "LightBlue",
+        "GreenYellow",
+        "PaleTurquoise",
+        "LightSteelBlue",
+        "PowderBlue",
+        "FireBrick",
+        "DarkGoldenRod",
+        "MediumOrchid",
+        "RosyBrown",
+        "DarkKhaki",
+        "Silver",
+        "MediumVioletRed",
+        "IndianRed",
+        "Peru",
+        "Chocolate",
+        "Tan",
+        "LightGray",
+        "LightGrey",
+        "Thistle",
+        "Orchid",
+        "GoldenRod",
+        "PaleVioletRed",
+        "Crimson",
+        "Gainsboro",
+        "Plum",
+        "BurlyWood",
+        "LightCyan",
+        "Lavender",
+        "DarkSalmon",
+        "Violet",
+        "PaleGoldenRod",
+        "LightCoral",
+        "Khaki",
+        "AliceBlue",
+        "HoneyDew",
+        "Azure",
+        "SandyBrown",
+        "Wheat",
+        "Beige",
+        "WhiteSmoke",
+        "MintCream",
+        "GhostWhite",
+        "Salmon",
+        "AntiqueWhite",
+        "Linen",
+        "LightGoldenRodYellow",
+        "OldLace",
+        "Red",
+        "Fuchsia",
+        "Magenta",
+        "DeepPink",
+        "OrangeRed",
+        "Tomato",
+        "HotPink",
+        "Coral",
+        "DarkOrange",
+        "LightSalmon",
+        "Orange",
+        "LightPink",
+        "Pink",
+        "Gold",
+        "PeachPuff",
+        "NavajoWhite",
+        "Moccasin",
+        "Bisque",
+        "MistyRose",
+        "BlanchedAlmond",
+        "PapayaWhip",
+        "LavenderBlush",
+        "SeaShell",
+        "Cornsilk",
+        "LemonChiffon",
+        "FloralWhite",
+        "Snow",
+        "Yellow",
+        "LightYellow",
+        "Ivory",
+        "White",
+      ],
+    };
+  },
+  methods: {
+    deleteFromList(index) {
+      this.list.splice(index, 1);
+    },
+  },
+  computed: {
+    filterColor: function () {
+      return this.list.filter((item) => {
+        return item.match(this.search);
+      });
+    },
+  },
+};
+</script>
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+// import 'expose?$!expose?jQuery!jquery'
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+//import css files
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
